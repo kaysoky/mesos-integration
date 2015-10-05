d8727427badf524bcc95e20e14deb3bd298704ec
Joseph Wu <joseph@mesosphere.io>
2015-10-05 12:06:53 -0700

    Improve test cleanup and add Agent init.

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import atexit
+import json
 import os
 import requests
 import subprocess
@@ -8,52 +10,126 @@ import unittest
 
 MESOS_BIN_PATH = 'MESOS_BIN_PATH'
 MESOS_MASTER_BIN = 'mesos-master.sh'
+MESOS_AGENT_BIN = 'mesos-slave.sh'
+
+
+CLEANUP_LAMBDAS = []
+def register_exit(func):
+    """
+    Stores a lambda in a stack for cleanup later.
+    All lambdas should be idempotent.
+    """
+
+    atexit.register(func)
+    CLEANUP_LAMBDAS.append(func)
+
+
+def cleanup():
+    """Calls all cleanup lambdas."""
+    while len(CLEANUP_LAMBDAS):
+        CLEANUP_LAMBDAS.pop()()
+
 
 def mesos_path():
     """Returns the MESOS_BIN_PATH environment variable."""
     return os.environ.get(MESOS_BIN_PATH)
 
-class TestIncomplete(unittest.TestCase):
-    """Incomplete."""
 
-    def setUp(self):
-        """Starts a Master and an Agent."""
-        self.work_dir = tempfile.mkdtemp(prefix='mesos-integration-')
+def start_master(work_dir, flags=[]):
+    """Starts a Master with the given flags."""
+    print 'Starting Mesos master at %s' % work_dir
+    stdout = open(os.path.join(work_dir, 'stdout.txt'), 'w')
+    stderr = open(os.path.join(work_dir, 'stderr.txt'), 'w')
+    master = subprocess.Popen(
+        [os.path.join(mesos_path(), MESOS_MASTER_BIN),
+         '--work_dir=%s' % work_dir] + flags,
+        stdin=None,
+        stdout=stdout,
+        stderr=stderr)
 
-        print 'Starting Mesos master at %s' % self.work_dir
-        stdout = open(os.path.join(self.work_dir, 'stdout.txt'), 'w')
-        stderr = open(os.path.join(self.work_dir, 'stderr.txt'), 'w')
-        self.master = subprocess.Popen(
-            [os.path.join(mesos_path(), MESOS_MASTER_BIN),
-             '--work_dir=%s' % self.work_dir],
-            stdin=None,
-            stdout=stdout,
-            stderr=stderr)
+    register_exit(lambda: master.kill())
+    register_exit(lambda: stdout.close())
+    register_exit(lambda: stderr.close())
 
-        print 'Waiting for Mesos master...'
-        timeout = 15
-        while timeout:
-            try:
-                result = requests.get('http://localhost:5050/master/state.json')
-                if result.status_code == 200:
+
+def wait_for_master(timeout=15, is_ssl=False):
+    """Waits for a Master to start."""
+    while timeout:
+        try:
+            result = requests.get('http%s://localhost:5050/master/state.json' % ('s' if is_ssl else ''))
+            if result.status_code == 200:
+                break
+        except requests.ConnectionError:
+            pass
+
+        time.sleep(1)
+        timeout -= 1
+
+    return timeout > 0
+
+
+def start_agent(work_dir, flags=[]):
+    """Starts an Agent with the given flags."""
+    print 'Starting Mesos agent'
+    stdout = open(os.path.join(work_dir, 'a_stdout.txt'), 'w')
+    stderr = open(os.path.join(work_dir, 'a_stderr.txt'), 'w')
+    agent = subprocess.Popen(
+        [os.path.join(mesos_path(), MESOS_AGENT_BIN),
+         '--master=10.141.141.1:5050'] + flags,
+        stdin=None,
+        stdout=stdout,
+        stderr=stderr)
+
+    register_exit(lambda: agent.kill())
+    register_exit(lambda: stdout.close())
+    register_exit(lambda: stderr.close())
+
+
+def wait_for_agent(num_agents=1, timeout=15, is_ssl=False):
+    """Waits for an Agent to start."""
+    while timeout:
+        try:
+            result = requests.get('http%s://localhost:5050/master/state.json' % ('s' if is_ssl else ''))
+            if result.status_code == 200:
+                result = result.json()
+                if len(result['slaves']) == num_agents and all(map(lambda x: x['active'], result['slaves'])):
                     break
-                print result.text
-            except requests.ConnectionError:
-                pass
+        except requests.ConnectionError:
+            pass
 
-            time.sleep(1)
-            timeout -= 1
+        time.sleep(1)
+        timeout -= 1
 
-        if not timeout:
-            self.tearDown()
+    return timeout > 0
+
+
+class MasterAgentTest(unittest.TestCase):
+    """Starts a Master and an Agent."""
+
+    @classmethod
+    def setUpClass(cls):
+        work_dir = tempfile.mkdtemp(prefix='mesos-integration-')
+
+        start_master(work_dir)
+        if not wait_for_master():
             self.fail('Master failed to start in time.')
 
-    def test_something(self):
+        start_agent(work_dir)
+        if not wait_for_agent():
+            self.fail('Agent failed to start in time.')
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cleanup()
+
+
+    def test_foo(self):
         print 'TODO...'
 
-    def tearDown(self):
-        """Stops the Master and Agent."""
-        self.master.kill()
+
+    def test_bar(self):
+        print 'TODO...'
 
 if __name__ == '__main__':
     if MESOS_BIN_PATH not in os.environ:
